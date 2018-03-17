8f7a62457370c5546fb75bcc88cf467668f4bbf7
Gavin Ong <gavinong10@gmail.com>
2018-03-17 14:28:46 -0700

    Alter cost function weights

--- ptg.py
+++ ptg.py
@@ -5,9 +5,9 @@ from constants import *
 
 # TODO - tweak weights to existing cost functions
 WEIGHTED_COST_FUNCTIONS = [
-    (time_diff_cost,    1),
+    (time_diff_cost,    10),
     (s_diff_cost,       1),
-    (d_diff_cost,       1),
+    (d_diff_cost,       10),
     (efficiency_cost,   1),
     (max_jerk_cost,     1),
     (total_jerk_cost,   1),
